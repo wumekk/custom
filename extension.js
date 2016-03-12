f6d9eee490c9f89b7fd1e91d093831fcf6a8ff3c
WorstUdyrDE <WorstUdyrDE@users.noreply.github.com>
2016-03-12 13:25:33 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,8 +1,5 @@
 (function () {
 
-    // Change this to your GitHub username so you don't have to modify so many things.
-    var fork = "bscBot";
-
     // Define our function responsible for extending the bot.
     function extend() {
         // If the bot hasn't been loaded properly, try again in 1 second(s).
@@ -24,19 +21,187 @@
           window.bot.chatUtilities.spam.push(spamWords[i]);
         }
 
-        // Example code for a bot command:
-        bot.commands.baconCommand = {
-            command: 'bacon',  // The command to be called. With the standard command literal this would be: !bacon
-            rank: 'user', // Minimum user permission to use the command
-            type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-              functionality: function (chat, cmd) {
-                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                if (!bot.commands.executable(this.rank, chat)) return void (0);
+	 bot.commands._nightmode = {
+            command: 'nightmode',
+            rank: 'mod',
+            type: 'exact',
+            functionality: function(chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
+                if (!bot.commands.executable(this.rank, chat)) return void(0);
                 else {
-                  API.sendChat("/me Bacon!!!");
+                    bot.settings.timeGuard = !bot.settings.timeGuard;
+                    bot.settings.blacklistEnabled = !bot.settings.blacklistEnabled;
+                    bot.settings.historySkip = !bot.settings.historySkip;
+                    var tempstr = "TimeGuard ustawiono na: " + bot.settings.timeGuard + ', Blacklist: ' + bot.settings.blacklistEnabled + ', HistorySkip: ' + bot.settings.historySkip + '. Dziękuję, dobranoc.'; //I tak ma zostac bo to kuhwa nightmode. //(albo dzień dobry, mój bóg - Wumekk nie ma czasu żeby zrobić drugą komendę, albo nie jest ona potrzebna).';
+                    API.sendChat(tempstr);
                 }
-              }
-            };
+            }
+        };
+        
+    
+        bot.commands._ban = {
+            command: 'permaban',
+            rank: 'mod',
+            type: 'startsWith',
+            functionality: function(chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
+                if (!bot.commands.executable(this.rank, chat)) return void(0);
+                else {
+                    var msg = chat.message;
+                    if (msg.length === cmd.length) return API.sendChat(subChat(bot.chat.nouserspecified, {
+                        name: chat.un
+                    }));
+                    var name = msg.substr(cmd.length + 2);
+                    var user = bot.userUtilities.lookupUserName(name);
+                    if (typeof user === 'boolean') return API.sendChat(subChat(basicBot.chat.invaliduserspecified, {
+                        name: chat.un
+                    }));
+                    API.moderateBanUser(user.id, 1, API.BAN.PERMA);
+                }
+            }
+        };
+        
+
+        bot.commands._cycleCommand = { //pierwsze uruchomienie czasem nie dziala
+            command: 'cykl',
+            rank: 'mod',
+            type: 'exact',
+            functionality: function(chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
+                if (!bot.commands.executable(this.rank, chat)) return void(0);
+                else {
+                    if ($(".dj-cycle")[0] === undefined) $("#room-name").click();
+                    hnd = $(".dj-cycle")[0].className.toLowerCase();
+                    if (hnd === "dj-cycle option") {
+                        console.log("[Cycle] Wykryto \"Wl\", proba wylaczenia.");
+                        API.moderateDJCycle(false);
+                    } else {
+                        if (hnd === "dj-cycle option enabled") {
+                            console.log("[Cycle] Wykryto \"Wyl\", proba wlaczenia.");
+                            API.moderateDJCycle(true);
+                        } else API.sendChat("/me Nieznany status cyklu. Może to wystąpić przy pierwszym uruchamianiu komendy. Spróbuj jej użyć jeszcze raz. Jeśli błąd się powtarza, oznacza to, iż nie komenda działa i tak na razie zostanie.")
+                    }
+                    API.sendChat("/me @Mycka1337 @Mycka1337, pokaż bicka");
+                    hnd = [];
+                }
+            }
+        };
+        
+function spinSlots() {
+            var slotArray = [':lemon:',
+                ':tangerine:',
+                ':strawberry:',
+                ':pineapple:',
+                ':apple:',
+                ':grapes:',
+                ':watermelon:',
+                ':cherries:',
+                ':green_heart:',
+                ':bell:',
+                ':gem:'];
+            var slotValue = [1.5,
+            2,
+            2.5,
+            3,
+            3.5,
+            4,
+            4.5,
+            5,
+            5.5,
+            6,
+            6.5];
+            var rand = Math.floor(Math.random() * (slotArray.length));
+            return [slotArray[rand], slotValue[rand]];
+        }
+
+        function spinOutcome(bet, chat) {
+            var winnings;
+            var outcome1 = spinSlots();
+            var outcome2 = spinSlots();
+            var outcome3 = spinSlots();
+
+            //Fix bet if blank
+            if (bet == null || bet == "" || bet == " " || bet == "!slot" || bet == "!slots") {
+                bet = 1;
+            }
+
+            //Determine Winnings
+            if (outcome1[0] == outcome2[0] && outcome1[0] == outcome3[0]) {
+                winnings = Math.round(bet * outcome1[1]);
+                setTimeout(function () {
+                    API.sendChat("@" + chat.un + " OGM Noob lucker reported... Wygrywasz... Pffff i tak powiecie, że było ustawione :keepo:");
+                    API.moderateMoveDJ(chat.uid, 1);
+                    setTimeout(function () {
+                        API.moderateDeleteChat(chat.cid);
+                    }, 7 * 1000, chat.cid);
+                    return false;
+                }, 2000);
+            } else {
+                winnings = 0;
+            }
+
+            return [outcome1[0], outcome2[0], outcome3[0], winnings];
+        }
+
+        //slots
+        bot.commands.slotsCommand = {
+            command: ['slots', 'slot', 'losuj', 'los'], //The command to be called. With the standard command literal this would be: !slots
+            rank: 'user',
+            type: 'startsWith',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
+                if (!bot.commands.executable(this.rank, chat)) return void(0);
+                else {
+                    this.lastSlots = null;
+                    var u = bot.userUtilities.lookupUser(chat.uid);
+                    if (u.lastSlots !== null && (Date.now() - u.lastSlots) < 1 * 5 * 60 * 1000) {
+                        API.moderateDeleteChat(chat.cid);
+                        return void(0);
+                    } else {
+                        u.lastSlots = Date.now();
+                        var msg = chat.message;
+                        var space = msg.indexOf(' ');
+                        var player = chat.un;
+                        var bet = msg.substring(space + 1);
+                        bet = Math.round(bet);
+                        var updatedTokens;
+
+                        var outcome = spinOutcome(bet, chat);
+                        //updatedTokens = slotWinnings(outcome[3], player);
+
+                        //Display Slots
+                        if (space === -1 || bet == 1) {
+                            //Start Slots
+
+                            setTimeout(function () {
+                                API.sendChat("@" + chat.un + " Wylosowano: " + outcome[0] + outcome[1] + outcome[2] + ". Spróbuj ponownie za 5 minut.")
+                                setTimeout(function () {
+                                    API.moderateDeleteChat(chat.cid);
+                                }, 4 * 1000, chat.cid);
+                                return false;
+
+                            }, 1000);
+
+                        } else if (bet > 1) {
+                            //Start Slots
+
+                            setTimeout(function () {
+                                API.sendChat("@" + chat.un + " Wylosowano: " + outcome[0] + outcome[1] + outcome[2] + ". Spróbuj ponownie za 5 minut.")
+                                setTimeout(function () {
+                                    API.moderateDeleteChat(chat.cid);
+                                }, 4 * 1000, chat.cid);
+                                return false;
+                            }, 1000);
+
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+        };
+        
+ 
 
         // Load the chat package again to account for any changes
         bot.loadChat();
@@ -46,20 +211,20 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "basicBot",
-      language: "english",
-      chatLink: "https://rawgit.com/bscBot/source/master/lang/en.json",
-      scriptLink: "https://rawgit.com/bscBot/source/master/basicBot.js",
+      botName: "Bot",
+      language: "Polish",
+      chatLink: "https://rawgit.com/WorstUdyrDE/custom/master/lang/pl.json",
+      scriptLink: "https://rawgit.com/WorstUdyrDE/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
       startupEmoji: false, // true or false
       autowoot: true,
       autoskip: false,
-      smartSkip: true,
+      smartSkip: false,
       cmdDeletion: true,
       maximumAfk: 120,
-      afkRemoval: true,
+      afkRemoval: false,
       maximumDc: 60,
       bouncerPlus: true,
       blacklistEnabled: true,
@@ -70,9 +235,9 @@
       maximumCycletime: 10,
       voteSkip: false,
       voteSkipLimit: 10,
-      historySkip: false,
+      historySkip: true,
       timeGuard: true,
-      maximumSongLength: 10,
+      maximumSongLength: 6,
       autodisable: true,
       commandCooldown: 30,
       usercommandsEnabled: true,
@@ -90,9 +255,9 @@
       afkRankCheck: "ambassador",
       motdEnabled: false,
       motdInterval: 5,
-      motd: "Temporary Message of the Day",
+      motd: null,
       filterChat: true,
-      etaRestriction: false,
+      etaRestriction: true,
       welcome: true,
       opLink: null,
       rulesLink: null,
@@ -102,16 +267,14 @@
       website: null,
       intervalMessages: [],
       messageInterval: 5,
-      songstats: true,
+      songstats: false,
       commandLiteral: "!",
       blacklists: {
-        NSFW: "https://rawgit.com/bscBot/custom/master/blacklists/NSFWlist.json",
-        OP: "https://rawgit.com/bscBot/custom/master/blacklists/OPlist.json",
-        BANNED: "https://rawgit.com/bscBot/custom/master/blacklists/BANNEDlist.json"
+        BANNED: "https://rawgit.com/WorstUdyrDE/custom/master/blacklists/BANNEDlist.json"
       }
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/bscBot/source/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/WorstUdyrDE/source/master/basicBot.js", extend);
 
 }).call(this);
