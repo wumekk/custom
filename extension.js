c4fd6cd40e3b1a962157168e3ef40330218a5a6b
WorstUdyrDE <WorstUdyrDE@users.noreply.github.com>
2016-03-17 21:19:54 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -200,7 +200,21 @@ function spinSlots() {
                 }
             }
         };
-        
+  
+        bot.commands.mehCommand = {
+            command: 'meh',
+            rank: 'manager',
+            type: 'exact',
+            functionality: function(chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
+                if (!bot.commands.executable(this.rank, chat)) return void(0);
+                else {
+                    eventDjadvance: function (obj) {
+                    	$("#meh").click();
+                    }
+                }
+            }
+        };       
  bot.commands.reloadCommand = [];
 
         // Load the chat package again to account for any changes
