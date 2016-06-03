786fe2731c695727fb7e4067c919c855a75a705c
WorstUdyrDE <WorstUdyrDE@users.noreply.github.com>
2016-06-03 17:08:20 +0200

    better work now

--- extension.js
+++ extension.js
@@ -69,7 +69,8 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
                 if (!bot.commands.executable(this.rank, chat)) return void(0);
                 else {
-                    API.sendChat("/me Rozszerzenie, które daje Ci możliwość używania emotikon z twitcha i wielu innych serwisów, Autojoin który dołącza za Ciebie do kolejki, Autowoot, specjalny wygląd community i wiele wiele więcej. Zainstaluj, a przekonasz się sam: https://rcs.radiant.dj");
+                    API.sendChat("/me Rozszerzenie, które daje Ci możliwość używania emotikon z twitcha i wielu innych serwisów, Autojoin który dołącza za Ciebie do kolejki, Autowoot, specjalny wygląd community i wiele wiele więcej.");
+                    API.sendChat("/me  Zainstaluj, a przekonasz się sam: https://rcs.radiant.dj");
                 }
             }
         };
